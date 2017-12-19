daa29f7c79539aac0e2fbdd8af474c022aa5d99e
HTPC <lrounin@gmail.com>
2017-12-18 18:45:48 -0700

    Add basic tile instantiation, add unitychan

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject tilePrefab;
+	public Transform stageCenter;
+	public float tileStartHeight;
+	public int stageSize;
+	public float tileSize;
+
+	
+	void Start () {
+		CreateTiles();	
+	}
+	
+	void CreateTiles() {
+
+		for (int x = 0; x < stageSize; x++) {
+			for (int z = 0; z < stageSize; z++) { 
+				float tileX = -((stageSize/2 - z) * tileSize) + tileSize / 2;
+				float tileY = -((stageSize/2 - x) * tileSize) + tileSize / 2;
+
+				float height = 0;
+
+				Vector3 tilePos = new Vector3(tileX, tileStartHeight, tileY);
+
+				GameObject tileInstance = Instantiate(
+					tilePrefab, 
+					tilePos, 
+					Quaternion.identity
+				) as GameObject;
+		
+				RaycastHit hit;
+				if (Physics.Raycast(
+					tileInstance.transform.position, 
+					-tileInstance.transform.up, 
+					out hit, 
+					Mathf.Infinity
+				)) {
+					Debug.Log("Raycast hit");
+					height = hit.point.y;
+					tileInstance.transform.position = new Vector3(
+						tileInstance.transform.position.x,
+						height + 0.001f,
+						tileInstance.transform.position.z
+					);
+				}
+
+				Tile tileScript = tileInstance.GetComponent<Tile>();
+				tileScript.setX(x);
+				tileScript.setZ(z);
+				tileScript.setHeight(height);
+
+				tileInstance.name = "Tile " + x + "," + z;
+			}
+		}
+
+
+
+	}
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+	private int x;
+	private int z;
+	private float height;
+
+	public int getX() { return x; }
+
+	public int getZ() {	return z; }
+
+	public float getHeight() { return height; }
+
+	public void setX(int newX) { x = newX; }
+
+	public void setZ(int newZ) { z = newZ; }
+
+	public void setHeight(float newHeight) { height = newHeight; }
+}
